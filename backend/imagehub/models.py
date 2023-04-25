0414be5c214a99b94cb7266ca16c1a9901983920
Rafał Piotrowicz <rafalp11500@gmail.com>
2023-04-25 19:50:51 +0200

    update models to new database schema

--- backend/imagehub/models.py
+++ backend/imagehub/models.py
@@ -4,7 +4,6 @@ from users.models import Account
 
 
 class Post(models.Model):
-    post_id = models.IntegerField(primary_key=True)
     user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
     title = models.CharField(max_length=45)
     description = models.TextField()
@@ -14,23 +13,27 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    comment_id = models.IntegerField(primary_key=True)
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
     comment_text = models.TextField()
     create_date = models.DateField()
-    parrent_comment = models.IntegerField()
 
 
-class Post_like(models.Model):
-    like_id = models.IntegerField(primary_key=True)
+class Subcomment(models.Model):
+    parrent_comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+    create_date = models.DateField()
+    
+
+class PostLike(models.Model):
     like_type = models.BooleanField()
     user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
-class Comment_like(models.Model): # Klasy nazywamy dużymi literami np. CommnetLike
-    like_id = models.IntegerField(primary_key=True)
-    like_type = models.BooleanField()
-    user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
-    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE)
+# class CommentLike(models.Model): 
+#     like_type = models.BooleanField()
+#     user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+#     comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE)
