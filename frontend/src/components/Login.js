7b0cd1d79f7d97e5fbdb85ad287c20f5c646afe2
Stanisław Olszak <solszakdev@gmail.com>
2023-04-27 00:37:06 +0200

    api modifications and token handling

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,45 +1,41 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import Header from './Header';
 import '../style/register.css'
 
-var acc="";
-function Login() {
-  const [accessToken, setAccessToken] = useState('');
+// zwraca promise z tokenem
+async function userLogin(credentials) {
+    return fetch('http://127.0.0.1:8000/api/token/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(credentials)
+    });  
+}
+
+export default function Login({setToken}) {
+  // const [accessToken, setAccessToken] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  
-  async function handleLogin(e) {
-    e.preventDefault();
-    //setError(Validation(values));
-    try {
-      const response = await fetch('http://127.0.0.1:8000/api/token/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      });
-      const data = await response.json();
-      if (!response.ok) {
-        throw new Error(data.message || 'DING DONG SOMETHING IS WRONG!!!!!!!!!!!!!!!!!!!!!!');
-      }
-      setAccessToken(data.accessToken);
-      acc+="Zalogowano. Twój access token to: ";
-      acc+=data.access;
-      // console.log(acc);
-    } catch (error) {
-      alert(error.message);
-    }
-    
+  // po wcisnieciu submita uruchamia userLogin i ustawia token
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const response = await userLogin({
+      email, 
+      password
+    });
+    const data = await response.json();
+    const token = data.access;
+    setToken(token);
   }
-  console.log(acc);
+
+  // console.log(acc);
   return (
     <div className='container'>
       <Header/>
       <div className='register'>
-        <form onSubmit={handleLogin}>
+        <form onSubmit={handleSubmit}>
           <label htmlFor= "email" id='login' className='registerForm'>
             <input type="text" name="login" className='registerForm' placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
           </label><br></br>
@@ -50,19 +46,17 @@ function Login() {
           <br></br>
 
           <div className="recoverPassword">
-            <Link to="/password">Forgot your password?</Link>
+            {/* <Link to="/password">Forgot your password?</Link> */}
             </div>
 
           <button type="submit" className='registerForm'>Sign in</button>
 
-          <Link to="/Register" className="signIn">Create an account</Link>
+          {/* <Link to="/Register" className="signIn">Create an account</Link> */}
         </form>
         
       </div>
-      <div><p>{acc}</p></div>
     </div>
     
   );
 };
 
-export default Login;
