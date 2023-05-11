27fc40831e26107ae0b548812cd1422eec1fdb72
Jacek <jtarzanowski@gmail.com>
2023-05-11 10:38:39 +0200

    reset-password_without_email

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(app_urls))
+    path('api/', include(app_urls)),
+    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
