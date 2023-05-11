6bbfc6cbd8e3191bf506b47bc8238c6321cdd108
Stanisław Olszak <solszakdev@gmail.com>
2023-05-11 11:46:17 +0200

    fixing store token error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,68 @@
 import Register from "./components/Register";
 import RecoverPassword from "./components/RecoverPassword";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
-import useToken from "./components/useToken";
+// import useToken from "./components/useToken";
 import Dashboard from "./components/Dashboard/Dashboard";
 
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
+// function setToken(userToken) {
+//   sessionStorage.setItem('token', JSON.stringify(userToken));
+// }
 
-function getToken() {
-  const tokenSring = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenSring);
-  return userToken?.token;
+// function getToken() {
+//   const tokenSring = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenSring);
+//   return userToken?.token;
+// }
+
+function useToken() {
+  let pair = []
+  const getToken = () => {
+      const tokenString = localStorage.getItem('token');
+      const userToken = JSON.parse(tokenString);
+      return userToken?.access
+  };
+
+  const [token, setToken] = useState(getToken());
+
+  // funckcja ustawia token w local storage i zarazem ustawia go jako stan komponentu
+  function saveToken(userToken) {
+      localStorage.setItem('token', JSON.stringify(userToken));
+      setToken(userToken.access);
+  };
+
+
+  pair.push(token, saveToken);
+  return pair;
 }
 
 const App = () => {
-  const token = getToken();
-  // const {token, setToken} = useToken();
-  if(!token) {
+  // const token = getToken();
+  const [token, setAccessToken] = useToken();
+
+  // useEffect(() => {
+
+  // }, [token])
+  if (token) {
     return <><p>hallo</p></>
   }
 
 
+
   return (
-      <div className="App">
-        <h1>Main page</h1>
-        {/* <p>{sessionStorage.getItem('token')}</p> */}
-        <Router>
-          <Routes>
-            <Route path="/" element={<Dashboard/>} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/password" element={<RecoverPassword />} />
-            <Route path="/login" element={<Login setToken={setToken} />} />
-          </Routes>
-        </Router>
-      </div>
+    <div className="App">
+      <h1>Main page</h1>
+      {/* <p>{sessionStorage.getItem('token')}</p> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/password" element={<RecoverPassword />} />
+          <Route path="/login" element={<Login setAccessToken={setAccessToken} />} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,16 +4,16 @@ import '../style/register.css'
 
 // zwraca promise z access oraz refresh tokenem
 async function userLogin(credentials) {
-    return fetch('http://127.0.0.1:8000/api/token/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
-    });  
+  return fetch('http://127.0.0.1:8000/api/token/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  });
 }
 
-export default function Login({setToken}) {
+export default function Login({ setAccessToken }) {
   // const [accessToken, setAccessToken] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -29,42 +29,41 @@ export default function Login({setToken}) {
     event.preventDefault();
 
     const response = await userLogin({
-      email, 
+      email,
       password
     });
     const data = await response.json();
     // pobieram access token z jsona jako objekt js
-    const token = data.access;
-    setToken(token);
+    setAccessToken(data.access);
   }
 
   // console.log(acc);
   return (
     <div className='container'>
-      <Header/>
+      <Header />
       <div className='register'>
         <form onSubmit={handleSubmit}>
-          <label htmlFor= "email" id='login' className='registerForm'>
-            <input type="text" name="login" className='registerForm' placeholder='email' onChange={(e) => setEmail(e.target.value)}/>
+          <label htmlFor="email" id='login' className='registerForm'>
+            <input type="text" name="login" className='registerForm' placeholder='email' onChange={(e) => setEmail(e.target.value)} />
           </label><br></br>
 
           <label htmlFor="password" id='password' className='registerForm'>
-            <input type="password" name="password" className='registerForm' placeholder='password' onChange={(e) => setPassword(e.target.value)}/>
+            <input type="password" name="password" className='registerForm' placeholder='password' onChange={(e) => setPassword(e.target.value)} />
           </label>
           <br></br>
 
           <div className="recoverPassword">
             {/* <Link to="/password">Forgot your password?</Link> */}
-            </div>
+          </div>
 
           <button type="submit" className='registerForm'>Sign in</button>
 
           {/* <Link to="/Register" className="signIn">Create an account</Link> */}
         </form>
-        
+
       </div>
     </div>
-    
+
   );
 };
 
