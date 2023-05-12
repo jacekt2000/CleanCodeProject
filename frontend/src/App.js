22789b0890c26ae23862522a420918fd37cf05f6
Stanisław Olszak <solszakdev@gmail.com>
2023-05-12 12:23:02 +0200

    new login component with axios services

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import Register from "./components/Register";
 import RecoverPassword from "./components/RecoverPassword";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
+import Login2 from "./components/Login2";
 // import useToken from "./components/useToken";
 import Dashboard from "./components/Dashboard/Dashboard";
 
@@ -16,37 +16,39 @@ import Dashboard from "./components/Dashboard/Dashboard";
 //   return userToken?.token;
 // }
 
-function useToken() {
-  let pair = []
-  const getToken = () => {
-      const tokenString = localStorage.getItem('token');
-      const userToken = JSON.parse(tokenString);
-      return userToken?.access
-  };
+// function
 
-  const [token, setToken] = useState(getToken());
+// function useToken() {
+//   let pair = []
+//   const getToken = () => {
+//     const tokenString = localStorage.getItem('token');
+//     const userToken = JSON.parse(tokenString);
+//     return userToken?.access;
+//   };
 
-  // funckcja ustawia token w local storage i zarazem ustawia go jako stan komponentu
-  function saveToken(userToken) {
-      localStorage.setItem('token', JSON.stringify(userToken));
-      setToken(userToken.access);
-  };
+//   const [token, setToken] = useState(getToken());
+
+//   // funckcja ustawia token w local storage i zarazem ustawia go jako stan komponentu
+//   function saveToken(userToken) {
+//     localStorage.setItem('token', JSON.stringify(userToken));
+//     setToken(userToken.access);
+//   };
 
 
-  pair.push(token, saveToken);
-  return pair;
-}
+//   pair.push(token, saveToken);
+//   return pair;
+// }
 
 const App = () => {
   // const token = getToken();
-  const [token, setAccessToken] = useToken();
+  // const [token, setAccessToken] = useToken();
 
   // useEffect(() => {
 
   // }, [token])
-  if (token) {
-    return <><p>hallo</p></>
-  }
+  // if (token) {
+  //   return <><p>hallo</p></>
+  // }
 
 
 
@@ -59,7 +61,7 @@ const App = () => {
           <Route path="/" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
           <Route path="/password" element={<RecoverPassword />} />
-          <Route path="/login" element={<Login setAccessToken={setAccessToken} />} />
+          <Route path="/login" element={<Login2 />} />
         </Routes>
       </Router>
     </div>
