24c70a54817ed064486d500126852455d7b2971d
Stanisław Olszak <solszakdev@gmail.com>
2023-05-16 01:39:21 +0200

    removal duble Link import

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     const imageLogo = <img style={{ height:180}} src={require('../images/logo3.png')} alt="" />
