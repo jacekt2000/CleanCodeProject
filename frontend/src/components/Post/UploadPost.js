bf983633cba658b8282ecbab97a8f5d2d9efce3c
Stanisław Olszak <solszakdev@gmail.com>
2023-06-01 13:48:08 +0200

    little router change

--- frontend/src/components/Post/UploadPost.js
+++ frontend/src/components/Post/UploadPost.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../../style/uploadPost.css';
 import HeaderSmall from '../HeaderSmall';
@@ -6,45 +6,53 @@ import HeaderSmall from '../HeaderSmall';
 
 export default function UploadPost() {
     const [data, setData] = useState({
-        user_id: 1,
+        // user_id: null,
         title: '',
         description: '',
         image: null,
         tag: ''
     });
 
+    // const handleChange = (e) => {
+    //     setData({
+    //         [e.target.id]: e.target.value
+    //     })
+    // };
+
     const handleChange = (e) => {
-        setData({
-            [e.target.id]: e.target.value
-        })
+        // const { id, value } = e.target;
+        setData((prevState) => ({ ...prevState, [e.target.id]: e.target.value }));
     };
 
     const handleImageChange = (e) => {
-        setData({
-            [image]: e.target.files[0]
-        })
+        setData((prevState) => ({
+            ...prevState,
+            image: e.target.files[0]
+        }));
     };
+
+    useEffect(() => {
+        console.log(data);
+    }, [data])
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(data);
+
         let form_data = new FormData();
-        form_data.append('user_id', 1);
-        form_data.append('create_date', '2023-05-16');
+        // form_data.append('user_id', data.user_id);
         form_data.append('title', data.title);
-        form_data.append('description', data.content);
-        form_data.append('image', data.image, data.image.name);
+        form_data.append('description', data.description);
+        form_data.append('image', data.image);
         form_data.append('tag', data.tag);
 
-        let url = 'http://localhost:8000/api/posts/';
+
+        const url = 'http://localhost:8000/api/posts/';
         axios.post(url, form_data, {
             headers: {
                 'content-type': 'multipart/form-data'
             }
         })
-            .then(res => {
-                console.log(res.data);
-            })
+            .then((res) => console.log(res.data))
             .catch(err => console.log(err))
     };
 
@@ -68,7 +76,7 @@ export default function UploadPost() {
 
                         <input type="file"
                             id="image"
-                            accept="image/png, image/jpeg, image/gif" className='uploadInput' onChange={handleImageChange} required />
+                            accept="image/png, image/jpeg" className='uploadInput' onChange={handleImageChange} required />
 
                     </p>
 
